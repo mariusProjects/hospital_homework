e2ec91023d5271e44c753f9cb1fe8ce0d3b79fa4
marius <mariusvdascalu@gmail.com>
2019-10-11 13:52:14 +0300

    Reorganizare proiect

--- src/main/java/com/sda/dao/AppointmentDao.java
+++ src/main/java/com/sda/dao/AppointmentDao.java
@@ -1,4 +1,19 @@
 package com.sda.dao;
 
-public class AppointmentDao {
+import com.sda.entities.Appointment;
+import com.sda.entities.Nurse;
+import com.sda.entities.Patient;
+import com.sda.entities.Physician;
+
+import java.sql.Timestamp;
+
+public class AppointmentDao extends GenericDao<Appointment> {
+    public void addAppointment(Patient patient, Nurse prepnurse, Physician physician, Timestamp startDateTime,Timestamp endDateTime ){
+        Appointment appointment = new Appointment();
+        appointment.setPatient(patient);
+        appointment.setPrepnurse(prepnurse);
+        appointment.setPhysician(physician);
+        appointment.setStartDateTime(startDateTime);
+        appointment.setEndDateTime(endDateTime);
+    }
 }
